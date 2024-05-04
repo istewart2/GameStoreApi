35170fda6fac4a570fa912c879e0fcc79c9409f1
Iain Stewart <iain.stewart08@gmail.com>
2024-05-04 12:01:04 +0100

    Add interface for games repo + inject into endpoints

--- GameStore.Api/Endpoints/GamesEndpoints.cs
+++ GameStore.Api/Endpoints/GamesEndpoints.cs
@@ -9,26 +9,24 @@ public static class GamesEndpoints
 
     public static RouteGroupBuilder MapGamesEndpoints(this IEndpointRouteBuilder routes) 
     {
-        InMemoryGamesRepository repository = new();
-
         var group = routes.MapGroup("/games").WithParameterValidation();
 
-        group.MapGet("/", () => repository.GetAll());
+        group.MapGet("/", (IGamesRepository repository) => repository.GetAll());
 
-        group.MapGet("/{id}", (int id) => 
+        group.MapGet("/{id}", (IGamesRepository repository, int id) => 
         {
             Game? game = repository.Get(id);
             return game is not null ? Results.Ok(game) : Results.NotFound();
         }).WithName(GetGameEndpoint);
 
-        group.MapPost("/", (Game game) => 
+        group.MapPost("/", (IGamesRepository repository, Game game) => 
         {
             repository.Create(game);
 
             return Results.CreatedAtRoute(GetGameEndpoint, new { id = game.Id }, game);
         });
 
-        group.MapPut("/{id}", (int id, Game updatedGame) =>
+        group.MapPut("/{id}", (IGamesRepository repository, int id, Game updatedGame) =>
         {
             Game? existingGame = repository.Get(id);
 
@@ -48,7 +46,7 @@ public static class GamesEndpoints
             return Results.NoContent();
         });
 
-        group.MapDelete("/{id}", (int id) => 
+        group.MapDelete("/{id}", (IGamesRepository repository, int id) => 
         {
                 Game? game = repository.Get(id);
 
--- GameStore.Api/Repositories/InMemoryGamesRepository.cs
+++ GameStore.Api/Repositories/InMemoryGamesRepository.cs
@@ -2,7 +2,7 @@ using GameStore.Api.Entities;
 
 namespace GameStore.Api.Repositories;
 
-public class InMemoryGamesRepository
+public class InMemoryGamesRepository : IGamesRepository
 {
     private readonly List<Game> games = new()
     {
