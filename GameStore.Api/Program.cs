af307a99697acc6eebf7e6f73b3dd9c58e684fcd
Iain Stewart <iain.stewart08@gmail.com>
2024-05-05 13:40:53 +0100

    Update connection string

--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -4,6 +4,8 @@ using GameStore.Api.Repositories;
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddSingleton<IGamesRepository, InMemoryGamesRepository>();
 
+builder.Configuration.GetConnectionString("GameStoreContext");
+
 var app = builder.Build();
 
 app.MapGamesEndpoints();
